8a2cdd22b7213db18a31086df33cd6f0cbd1a4ff
wxf <xuefei.wu@hotmail.com>
2022-01-01 14:11:16 +0800

    1. uncompress initrd.ext2.gz for write 2. add run scripts

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm bin/bbootsect bin/bsetup bin/build bin/LOG bin/bzImage bin/tccboot.iso 2> /dev/null 
+rm bin/bbootsect bin/bsetup bin/build bin/LOG bin/vmlinux bin/bzImage bin/tccboot.iso 2> /dev/null 
 [ -d temp ] && rm -r temp
 rm *.log 2> /dev/null
 git checkout -- bin/boot/grub/stage2_eltorito
